86ddc3d0965202186d1bb820f9ddb5522e21f9f1
Amaru Monje <amaru.monje@usach.cl>
2023-09-25 03:58:15 -0300

    Testeo/Funcion para extraer de lista

--- Proy_Funcional.rkt
+++ Proy_Funcional.rkt
@@ -1,1 +1,10 @@
 #lang Racket
+
+;testeo
+;(define Recibir_input (lambda (CB)
+;                        ((display "Test receive: ")
+;                         (display (read-line)))))
+
+(define SelectorLista (lambda (n L) (if (= n 0)
+                                        (car L)
+                                        (SelectorLista (- n 1) (cdr L)))))
