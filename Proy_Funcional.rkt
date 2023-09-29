6070a8c00821c474be9adf465040d6e805e436e0
Amaru Monje <amaru.monje@usach.cl>
2023-09-29 02:36:53 -0300

    Primer Constructor y testeo

--- Proy_Funcional.rkt
+++ Proy_Funcional.rkt
@@ -1,10 +1,30 @@
 #lang Racket
 
-;testeo
-;(define Recibir_input (lambda (CB)
-;                        ((display "Test receive: ")
-;                         (display (read-line)))))
-
 (define SelectorLista (lambda (n L) (if (= n 0)
                                         (car L)
                                         (SelectorLista (- n 1) (cdr L)))))
+
+(define InvalidOption null)
+
+(define all-strings? (lambda (lista)
+        (if (null? lista)
+            #t
+            (if (string? (car lista))
+                (all-strings? (cdr lista))
+                #f))))
+            
+
+;----------------Constructores TDA
+
+;Option
+(define Option (lambda (Code Message ChatbotCodeLink InitialFlowCodeLink . Keywords)
+        (if (and (integer? Code)(string? Message)(integer? ChatbotCodeLink)
+                 (integer? InitialFlowCodeLink))
+            (list Code Message ChatbotCodeLink InitialFlowCodeLink
+                  (if (and (not (null? Keywords)) (all-strings? Keywords))
+                          Keywords
+                          null))
+            InvalidOption)))
+
+;testing
+(display (Option 1 "Test" 1 1 "test2" "test3" "test4"))
