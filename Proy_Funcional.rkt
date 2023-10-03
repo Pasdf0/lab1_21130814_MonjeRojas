d924c64c78ec708f47c592bd82755f5df212ca34
Amaru Monje <amaru.monje@usach.cl>
2023-10-03 00:13:36 -0300

    duplicidad en chatbot y chatbot-add-flow terminados/system constructor

--- Proy_Funcional.rkt
+++ Proy_Funcional.rkt
@@ -1,8 +1,13 @@
 #lang Racket
+(provide (all-defined-out))
 
 (define InvalidOption null)
 (define InvalidFlow null)
 (define InvalidChatbot null)
+(define InvalidSystem null)
+
+(define epi? (lambda (n) (or (exact-positive-integer? n) (= n 0))))
+;Se redefine la función para verificar ids en caso de requerir cambios
 
 (define list-dup? (lambda (lista)
         (not (false? (check-duplicates lista)))))
@@ -15,53 +20,74 @@
 
 ;Option
 (define option (lambda (Code Message ChatbotCodeLink InitialFlowCodeLink . Keywords)
-        (if (and (exact-positive-integer? Code)(string? Message)(exact-positive-integer? ChatbotCodeLink)
-                 (exact-positive-integer? InitialFlowCodeLink))
+        (if (and (epi? Code)(string? Message)(epi? ChatbotCodeLink)(epi? InitialFlowCodeLink))
             (list Code Message ChatbotCodeLink InitialFlowCodeLink (if (all-strings? Keywords)
                                                                        Keywords null))
             InvalidOption)))
 
 ;Flow
 (define flow (lambda (Flow-ID msg . Options)
-        (if (and (exact-positive-integer? Flow-ID)(string? msg))
+        (if (and (epi? Flow-ID)(string? msg))
             (list Flow-ID msg (if (all-option? Options)
                                    (dup-list-op Options) null))
             InvalidFlow)))
 
 ;Chatbot
 (define chatbot (lambda (Chatbot-ID name welcome-msg startFlowID . Flows)
-        (if (and (exact-positive-integer? Chatbot-ID)(string? name)
-                 (string? welcome-msg)(exact-positive-integer? startFlowID))
+        (if (and (epi? Chatbot-ID)(string? name)(string? welcome-msg)(epi? startFlowID))
             (list Chatbot-ID name welcome-msg startFlowID (if (all-flow? Flows)
-                                                              Flows null))
+                                                              (dup-list-fw Flows) null))
             InvalidChatbot)))
+
+;System
+(define system (lambda (name InitialChatbotCodeLink . Chatbots)
+        (if (and (string? name)(epi? InitialChatbotCodeLink))
+            (list name InitialChatbotCodeLink (if (all-chatbot? Chatbots)
+                                                  (dup-list-cb Chatbots) null))
+            InvalidSystem)))
 
 ;---------------Funciones de pertenencia TDA
 
 ;Option
 (define option? (lambda (op)                  
-        (and (list? op)(= (length op) 5)(exact-positive-integer? (car op))(string? (cadr op))
-             (exact-positive-integer? (caddr op))(exact-positive-integer? (cadddr op))
-             (or (all-strings? (car (cddddr op)))(null? (car (cddddr op)))))))
+        (and (list? op)(= (length op) 5)(epi? (car op))(string? (cadr op))(epi? (caddr op))
+             (epi? (cadddr op))(all-strings? (car (cddddr op))))))
 
-(define all-option? (lambda (lista)
-        (if (null? lista)
+(define all-option? (lambda (list-op)
+        (if (null? list-op)
             #t
-            (if (option? (car lista))
-                (all-option? (cdr lista))
+            (if (option? (car list-op))
+                (all-option? (cdr list-op))
                 #f))))
 
 ;Flow
-(define flow? (lambda (fl)
-        (and (list? fl)(= (length fl) 3)(exact-positive-integer? (car fl))
-             (string? (cadr fl))(all-option? (caddr fl)))))
+(define flow? (lambda (fw)
+        (and (list? fw)(= (length fw) 3)(epi? (car fw))
+             (string? (cadr fw))(all-option? (caddr fw)))))
 
-(define all-flow? (lambda (lista)
-        (if (null? lista)
+(define all-flow? (lambda (list-fw)
+        (if (null? list-fw)
             #t
-            (if (flow? (car lista))
-                (all-flow? (cdr lista))
+            (if (flow? (car list-fw))
+                (all-flow? (cdr list-fw))
                 #f))))
+
+;Chatbot
+(define chatbot? (lambda (cb)
+        (and (list? cb)(= (length cb) 5)(epi? (car cb))(string? (cadr cb))
+             (string? (caddr cb))(epi? (cadddr cb))(all-flow? (car (cddddr cb))))))
+
+(define all-chatbot? (lambda (list-cb)
+        (if (null? list-cb)
+            #t
+            (if (chatbot? (cdr list-cb))
+                (all-chatbot? (cdr list-cb))
+                #f))))
+
+;System
+(define system? (lambda (sys)
+        (and (list? sys)(= (length sys) 3)(string? (car sys))
+             (epi? (cadr sys))(all-chatbot? (caddr sys)))))
 
 ;Adicionales
 
@@ -116,6 +142,48 @@
             (list-ref fw 2)
             null)))
 
+;Chatbot
+(define Sel-cb-id (lambda (cb)
+        (if (chatbot? cb)
+            (list-ref cb 0)
+            null)))
+
+(define Sel-cb-name (lambda (cb)
+        (if (chatbot? cb)
+            (list-ref cb 1)
+            null)))
+
+(define Sel-cb-welmsg (lambda (cb)
+        (if (chatbot? cb)
+            (list-ref cb 2)
+            null)))
+
+(define Sel-cb-startfwid (lambda (cb)
+        (if (chatbot? cb)
+            (list-ref cb 3)
+            null)))
+
+(define Sel-cb-fw (lambda (cb)
+        (if (chatbot? cb)
+            (list-ref cb 4)
+            null)))
+
+;System
+(define Sel-sys-name (lambda (sys)
+        (if (system? sys)
+            (list-ref sys 0)
+            null)))
+
+(define Sel-sys-startcbcode (lambda (sys)
+        (if (system? sys)
+            (list-ref sys 1)
+            null)))
+
+(define Sel-sys-cb (lambda (sys)
+        (if (system? sys)
+            (list-ref sys 2)
+            null)))
+
 ;------------------Duplicidad
 ;Sin Utilidad por ahora
 ;(define flow-dup-op? (lambda (fw)
@@ -133,7 +201,10 @@
                 (= (Sel-fw-id fw1) (Sel-fw-id fw2))))
         (remove-duplicates list-fw compare-fw-id)))
 
-
+(define dup-list-cb (lambda (list-cb)
+        (define compare-cb-id (lambda (cb1 cb2)
+                (= (Sel-cb-id cb1) (Sel-cb-id cb2))))
+        (remove-duplicates list-cb compare-cb-id)))
 
 
 ;-------------------Modificadores
@@ -146,12 +217,11 @@
             InvalidOption)))
 
 ;Flow
-(define delete-fw-op (lambda (fw)
-        (if (flow? fw)
-            (remove (Sel-fw-op fw) fw)
-            InvalidFlow)))
-
 (define change-fw-op (lambda (fw list-op)
+        (define delete-fw-op (lambda (fw)
+                (if (flow? fw)
+                    (remove (Sel-fw-op fw) fw)
+                    InvalidFlow)))
         (if (and (flow? fw)(all-option? list-op))
             (append (delete-fw-op fw) list-op)
             InvalidFlow)))
@@ -160,6 +230,22 @@
         (if (and (flow? fw)(option? op))
             (change-fw-op fw (dup-list-op (append (Sel-fw-op fw) (list op))))
             InvalidFlow)))
+
+;Chatbot
+(define change-cb-fw (lambda (cb list-fw)
+        (define delete-cb-fw (lambda (cb)
+                (if (chatbot? cb)
+                    (remove (Sel-cb-fw cb) cb)
+                    InvalidChatbot)))
+        (if (and (chatbot? cb)(all-flow? list-fw))
+            (append (delete-cb-fw cb) list-fw)
+            InvalidChatbot)))
+
+(define chatbot-add-flow (lambda (cb fw)
+        (if (and (chatbot? cb)(flow? fw))
+            (change-cb-fw cb (dup-list-fw (append (Sel-cb-fw cb) (list fw))))
+            InvalidChatbot)))
+
 
 ;-----------------------------------------------------------
 
@@ -174,4 +260,6 @@
 (define n2 (myRandom n1))
 ;(dup-list-op (Sel-fw-op fw1))
 (define test (dup-list-op (append (Sel-fw-op fw1) (list op4))))
-(flow-add-option fw1 op4)
+;(flow-add-option fw1 op4)
+
+
