c4d710293d8face990552bcebc24ecc75ec7e532
Amaru Monje <amaru.monje@usach.cl>
2023-10-09 04:08:15 -0300

    mitad documentacion

--- chatbot_21130814_MonjeRojas.rkt
+++ chatbot_21130814_MonjeRojas.rkt
@@ -6,10 +6,19 @@
 ;(require "chathistory_21130814_MonjeRojas.rkt")
 (provide (all-defined-out))
 
+;############### TDA Chatbot ###############
+
 (define InvalidChatbot null)
 
 ;--------------------Constructor
 
+; Funcionalidad 5 Constructor Chatbot
+
+;Dominio: Chatbot-ID(positive integer) x name(string) x welcome-msg(string) x startFlowID(positive integer) x Flows(List of flows)
+;Recorrido: Chatbot
+;Recursividad: No aplica
+;Descripción: Constructor de chatbot, comprueba que los tipos de datos de las entradas correspondan con lo que
+;             se requiere, si lo hacen, se crea un chatbot con estas.
 (define chatbot (lambda (Chatbot-ID name welcome-msg startFlowID . Flows)
         (if (and (epi? Chatbot-ID)(string? name)(string? welcome-msg)(epi? startFlowID))
             (list Chatbot-ID name welcome-msg startFlowID (if (all-flow? Flows)
@@ -18,35 +27,63 @@
 
 ;--------------------Pertenencia
 
+;Dominio: Chatbot
+;Recorrido: Boolean
+;Recursividad: No aplica
+;Descripción: Comprueba que el argumento entregado sea un chatbot.
 (define chatbot? (lambda (cb)
         (and (list? cb)(= (length cb) 5)(epi? (car cb))(string? (cadr cb))
              (string? (caddr cb))(epi? (cadddr cb))(all-flow? (car (cddddr cb))))))
 
+;Dominio: List
+;Recorrido: Boolean
+;Recursividad: No aplica
+;Descripción: Comprueba que todos los elementos de una lista sean chatbots.
 (define all-chatbot? (lambda (list-cb)
         (andmap chatbot? list-cb)))
 
 ;--------------------Selectores
 
+;Dominio: Chatbot
+;Recorrido: id (positive integer)
+;Recursividad: No aplica
+;Descripción: Selector de chatbot para su id.
 (define Sel-cb-id (lambda (cb)
         (if (chatbot? cb)
             (list-ref cb 0)
             null)))
 
+;Dominio: Chatbot
+;Recorrido: name (string)
+;Recursividad: No aplica
+;Descripción: Selector de chatbot para su nombre.
 (define Sel-cb-name (lambda (cb)
         (if (chatbot? cb)
             (list-ref cb 1)
             null)))
 
+;Dominio: Chatbot
+;Recorrido: welcome-msg (string)
+;Recursividad: No aplica
+;Descripción: Selector de chatbot para su mensaje de bienvenida.
 (define Sel-cb-welmsg (lambda (cb)
         (if (chatbot? cb)
             (list-ref cb 2)
             null)))
 
+;Dominio: Chatbot
+;Recorrido: startfwid (positive integer)
+;Recursividad: No aplica
+;Descripción: Selector de chatbot para la id de su flow actual.
 (define Sel-cb-startfwid (lambda (cb)
         (if (chatbot? cb)
             (list-ref cb 3)
             null)))
 
+;Dominio: Chatbot
+;Recorrido: Flows (list)
+;Recursividad: No aplica
+;Descripción: Selector de chatbot para obtener sus flows.
 (define Sel-cb-fw (lambda (cb)
         (if (chatbot? cb)
             (list-ref cb 4)
@@ -54,33 +91,61 @@
 
 ;--------------------Modificadores
 
+;Dominio: List of Chatbots (list)
+;Recorrido: List of Chatbots (list)
+;Recursividad: No aplica
+;Descripción: Elimina chatbot duplicados a partir de las ids.
 (define dup-list-cb (lambda (list-cb)
         (define compare-cb-id (lambda (cb1 cb2)
                 (= (Sel-cb-id cb1) (Sel-cb-id cb2))))
         (remove-duplicates list-cb compare-cb-id)))
 
-
+;Dominio: Chatbot x id(positive integer)
+;Recorrido: Flow
+;Recursividad: No aplica
+;Descripción: Busca un flow dentro de un chatbot a partir de su id.
 (define find-fw (lambda (cb id)
         (findf (lambda (arg) (= id (car arg))) (Sel-cb-fw cb))))
 
-(define change-cb-fw (lambda (cb list-fw)
-        (define delete-cb-fw (lambda (cb)
-                (remove (Sel-cb-fw cb) cb)))
-        (if (and (chatbot? cb)(all-flow? list-fw))
-            (append (delete-cb-fw cb) list-fw)
-            InvalidChatbot)))
-
+;Dominio: Chatbot x id(positive integer)
+;Recorrido: Chatbot
+;Recursividad: No aplica
+;Descripción: Cambia el startflowid de un chatbot.
 (define change-cb-startfwid (lambda (cb id)
         (if (and (chatbot? cb)(epi? id))
             (cons (Sel-cb-id cb) (cons (Sel-cb-name cb) (cons (Sel-cb-welmsg cb)
             (cons id (cddddr cb)))))
             InvalidChatbot)))
 
+;Dominio: Lista de chatbots (list) x Chatbot x id (positive integer)
+;Recorrido: Lista de chatbots (list)
+;Recursividad: No aplica
+;Descripción: Utiliza change-cb-startfwid para cambiar la startflowid de un chatbot y
+;             retornar la lista de chatbots completa, su funcion principal es
+;             actualizar un system para las funciones system-talk.
 (define change-list-cb-startfwid (lambda (list-cb cb id)
         (if (and (all-chatbot? list-cb)(chatbot? cb)(epi? id))
             (dup-list-cb (cons (change-cb-startfwid cb id) list-cb))
             InvalidChatbot)))
 
+; Funcionalidad 6 Chatbot add flow
+
+;Dominio: Chatbot x Lista de Flows (list)
+;Recorrido: Chatbot
+;Recursividad: No aplica
+;Descripción: Reemplaza la lista de flows de un chatbot.
+(define change-cb-fw (lambda (cb list-fw)
+        (define delete-cb-fw (lambda (cb)
+                (remove (Sel-cb-fw cb) cb)))
+        (if (and (chatbot? cb)(all-flow? list-fw))
+            (append (delete-cb-fw cb) (list list-fw))
+            InvalidChatbot)))
+
+;Dominio: Chatbot x Flow
+;Recorrido: Chatbot
+;Recursividad: Cola
+;Descripción: Usa cb-add-aux para añadir un flow a la lista de flows usando recursion de cola
+;             luego reemplaza la lista de flows obtenida en el chatbot indicado.
 (define chatbot-add-flow (lambda (cb fw)
         (define cb-add-aux (lambda (list-fw fw aux)
                 (if (null? list-fw)
--- main_21130814_MonjeRojas.rkt
+++ main_21130814_MonjeRojas.rkt
@@ -8,19 +8,20 @@
 (require "chathistory_21130814_MonjeRojas.rkt")
 (provide (all-defined-out))
 
-;(define epi? (lambda (n) (or (exact-positive-integer? n) (= n 0))))
-;Se redefine la función para verificar ids en caso de requerir cambios
-
-;(define list-dup? (lambda (lista)
-;        (not (false? (check-duplicates lista)))))
-
+;############### Main ###############
 
 (define (myRandom Xn)
         (modulo (+ (* 1103515245 Xn) 12345) 2147483648)
 )
 
-;--------------------System talk rec
+; Funcionalidad 12 System talk rec
 
+;Dominio: Flow x msg(string)
+;Recorrido: Option
+;Recursividad: Cola
+;Descripción: Busca si un msg esta en un flow, revisando cada option, sus keywords e ids,
+;             De ser encontrado se retorna la option que contiene el msg.
+;             Se utiliza funcion msg-in-op? contenida en TDA-System.
 (define msg-in-fw-rec (lambda (fw msg)
         (define msg-fw-aux (lambda (list-op msg)
                 (if (null? list-op)
@@ -30,14 +31,30 @@
                         (msg-fw-aux (cdr list-op) msg)))))
         (msg-fw-aux (Sel-fw-op fw) msg)))
 
+;Dominio: System x msg(string)
+;Recorrido: Option
+;Recursividad: Propia de msg-in-fw-rec (Cola)
+;Descripción: Encapsula función msg-in-fw-rec para realizar búsqueda en system
 (define msg-in-sys-rec (lambda (sys msg)
         (msg-in-fw-rec (get-current-fw sys) msg)))
 
+;Dominio: System x msg(string)
+;Recorrido: System
+;Recursividad: Propia de msg-in-sys-rec y msg-in-fw-rec (Cola)
+;Descripción: Funcion que actualiza el start-chatbot-code de un system, el chatbot-initialflowid del
+;             chatbot que actualizó la función anterior y finalmente agrega un registro al chathistory del
+;             user que está logueado en el system. Esto a partir del option obtenido en msg-in-sys-rec.
 (define sys-update-rec (lambda (sys msg)
         (sys-update-usr (sys-update-cb-fwid (sys-update-cbcode sys
         (Sel-op-cbcodelink (msg-in-sys-rec sys msg))) (Sel-op-fwcodelink (msg-in-sys-rec sys msg))) msg)))
 
-
+;Dominio: System x msg(string)
+;Recorrido: System
+;Recursividad: Propia de sys-update-rec (Cola)
+;Descripción: Permite interactuar con un system ingresando un string, este puede ser una keyword o un id.
+;             En caso de que el msg se encuentre en el estado actual del system se actualiza este último
+;             al nuevo estado, en caso contrario, se agrega el estado actual al chathistory del usuario
+;             actual.
 (define system-talk-rec (lambda (sys msg)
         (if (and (system? sys)(string? msg)(not (equal? (Sel-sys-usr sys) NoUser)))
             (if (msg-in-sys? sys msg)
@@ -45,18 +62,37 @@
                 (sys-update-usr sys msg))
             InvalidSystem)))
 
-;--------------------System talk norec
+; Funcionalidad 13 System talk norec
 
+;Dominio: Flow x msg(string)
+;Recorrido: Option
+;Recursividad: No aplica
+;Descripción: Misma funcion que msg-in-fw-rec pero sin recursividad, busca un option a partir de un msg.
 (define msg-in-fw-norec (lambda (fw msg)
         (findf (lambda (arg) (msg-in-op? arg msg)) (Sel-fw-op fw))))
 
+;Dominio: System x msg(string)
+;Recorrido: Option
+;Recursividad: No aplica
+;Descripción: Encapsula msg-in-fw-norec, busca option en un system a partir de un string.
 (define msg-in-sys-norec (lambda (sys msg)
         (msg-in-fw-norec (get-current-fw sys) msg)))
 
+;Dominio: System x msg(string)
+;Recorrido: System
+;Recursividad: No aplica
+;Descripción: Actualiza el start-chatbot-code de un system, el chatbot-initialflowid del
+;             chatbot y el chathistory del usuario logueado. Similar a system-update-rec pero
+;             sin utilizar recursividad.
 (define sys-update-norec (lambda (sys msg)
         (sys-update-usr (sys-update-cb-fwid (sys-update-cbcode sys
         (Sel-op-cbcodelink (msg-in-sys-rec sys msg))) (Sel-op-fwcodelink (msg-in-sys-norec sys msg))) msg)))
 
+;Dominio: System x msg
+;Recorrido: System
+;Recursividad: No aplica
+;Descripción: Permite interactuar con un system ingresando un msg, siempre que exista un usuario
+;             Logueado. Similar a system-talk-rec pero sin utilizar recursividad.
 (define system-talk-norec (lambda (sys msg)
         (if (and (system? sys)(string? msg)(not (equal? (Sel-sys-usr sys) NoUser)))
             (if (msg-in-sys? sys msg)
@@ -64,10 +100,21 @@
                 (sys-update-usr sys msg))
             InvalidSystem)))
 
-;--------------------System synthesis
+; Funcionalidad 14 System synthesis
+
+;Dominio: Lista de strings
+;Recorrido: string
+;Recursividad: No aplica
+;Descripción: Junta los strings de una lista agregando un salto de linea entre ellos, sirve para
+;             formatear las opciones almacenadas en el chathistory.
 (define str-append-cola (lambda (lista)
         (map (lambda (arg) (string-append arg "\n")) lista)))
 
+;Dominio: User
+;Recorrido: string
+;Recursividad: No aplica
+;Descripción: Formatea un chathistory juntando los strings y añadiendo saltos de linea, retorna un string
+;             listo para display
 (define format-sys (lambda (usr)
         (define format-aux (lambda (chathis)
                 (string-append (Sel-usr-name usr) ": " (car chathis) "\n"
@@ -75,6 +122,11 @@
                 (apply string-append (str-append-cola (cdddr chathis))) "\n\n")))
         (apply string-append (map format-aux (Sel-usr-his usr)))))
 
+;Dominio: System x usr-name(string)
+;Recorrido: string
+;Recursividad: No aplica
+;Descripción: Usando la función format-sys obtiene un string con el chathistory del usuario solicitado
+;             formateado para display.
 (define system-synthesis (lambda (sys usr-name)
         (if (and (system? sys)(string? usr-name))
             (if (usr-is-in (user usr-name) (Sel-sys-list-usr sys))
@@ -84,6 +136,7 @@
                 InvalidUser)
             InvalidUser)))
 
-;--------------------System simulate
+; Funcionalidad 15 System simulate
 
-
+;(define system-simulate (lambda (sys maxInter seed)
+        
