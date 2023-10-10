5f58bc6aa57741d23c08c86cd793bc8053981442
Amaru Monje <amaru.monje@usach.cl>
2023-10-09 23:51:07 -0300

    Cambios Finales

--- pruebas_21130814_MonjeRojas.rkt
+++ pruebas_21130814_MonjeRojas.rkt
@@ -67,23 +67,41 @@
 ;(system-simulate s0 5 32131) ;No se implementó
 
 ;Ejemplos complementarios
-;Se añaden ejemplos para los requerimientos funcionales que no tienen 3 o más ejemplos en la
-;parte anterior del script.
+;Se añaden 3 ejemplos por requerimiento funcional
 
+(define op01 (option 1 "1) test" 1 1 "test01" "test02" "test03"))
+(define op02 (option 2 "2) test" 1 1 "test04" 5 "test06"))
+(define op03 (option 5 "3) test" 1 1 "" "" ""))
+(define f01 (flow 1 1090382 op6 op7 op8 op9))
+(define f02 (flow 1 "Flow test" op01 op1 op2 op2))
+(define f03 (flow 1 "Empty Flow" ))
 (define f12 (flow-add-option f10 op18))
 (define f13 (flow-add-option f12 op17)) ;se elimina por duplicidad
 (define f31 (flow-add-option f30 op15))
+(define cb01 (chatbot 1 "InvalidChatbot" 26 1 f20 f20 f20 f20 f20 f20))
+(define cb02 (chatbot 2 "DuplicateChatbot" "test msg" 1 f30 f20 f10))
+(define cb03 (chatbot 3 "EmptyChatbot" "test msg 2" 25 ))
 (define cb3 (chatbot-add-flow cb0 f21))
 (define cb4 (chatbot-add-flow cb3 f22))
 (define cb5 (chatbot-add-flow cb2 f20)) ;se elimina por duplicidad
-(define s01 (system "Chatbot test" 1 cb0 cb3 cb5 cb5)) ;eliminará cb3 y un cb5 por duplicidad
-(define s02 (system "Chatbot test2" 25)) ;se crea sin chatbots
-(define s03 (system "Chatbot testfail" "not-id" cb0 cb1 cb2 cb3)) ;Retorna InvalidSystem
-(define s04 (system-add-chatbot s01 cb1)) 
-(define s05 (system-add-chatbot s02 cb5)) ;se agrega chatbot a system sin chatbots
-(define s06 (system-add-chatbot s0 cb5)) ;no se agrega por duplicidad de id
-(define s07 (system-logout s0)) ;Se hace logout en un system sin user logueado
-(define s08 (system-logout s7)) ;Se hace logout en un system sin chathistory en user activo
+(define s001 (system "Chatbot test" 1 cb0 cb3 cb5 cb5)) ;eliminará cb3 y un cb5 por duplicidad
+(define s002 (system "Chatbot test2" 25)) ;se crea sin chatbots
+(define s003 (system "Chatbot testfail" "not-id" cb0 cb1 cb2 cb3)) ;Retorna InvalidSystem
+(define s004 (system-add-chatbot s001 cb1)) 
+(define s005 (system-add-chatbot s002 cb5)) ;se agrega chatbot a system sin chatbots
+(define s006 (system-add-chatbot s0 cb5)) ;no se agrega por duplicidad de id
+(define s01 (system-add-user s0 123))
+(define s02 (system-add-user s0 NoUser))
+(define s03 (system-add-user s4 "tests03"))
+(define s04 (system-login s0 NoUser))
+(define s05 (system-login s0 "user1"))
+(define s06 (system-login s0 1))
+(define s07 (system-logout 1))
+(define s08 (system-logout s0)) ;Se hace logout en un system sin user logueado
+(define s09 (system-logout s7)) ;Se hace logout en un system sin chathistory en user activo
+(define s010 (system-talk-rec s07 "1"))
+(define s011 (system-talk-rec s17 "hola"))
+(define s012 (system-talk-rec s17 "1"))
 (define s11-norec (system-talk-norec s10 "hola"))
 (define s12-norec (system-talk-norec s11-norec "1"))
 (define s13-norec (system-talk-norec s12-norec "1"))
@@ -95,3 +113,4 @@
 (display "\nSystem-synthesis norec: \n\n")
 (display (system-synthesis s17-norec "user2")) ;Ejemplo system-synthesis con talk-norec
 (display (system-synthesis s10 "user1")) ;Ejemplo user sin historial, no se muestra nada.
+(system-synthesis s17 "user2")
